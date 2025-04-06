01531feeddcf6f38c00b20a2cd195e9023f7fc76
Markus Fischer <fischer@mideasdesign.de>
2025-04-06 22:37:08 +0200

    add some properties like weight,hieht to the project

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 let allPokemons = [];
 
 async function init() {
-    await fetchPokemons(); // Warten, bis die Daten geladen sind
-    renderPokemons();      // Danach erst rendern
+    const baseData = await fetchBasePokemons();
+    await fetchDetailedPokemons(baseData);
+    renderPokemons();
 }
 
-async function fetchPokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon-habitat');
+async function fetchBasePokemons() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=100');
     const data = await response.json();
-    allPokemons = data;
-    console.log(allPokemons);
+    return data.results; // Array mit { name, url }
+}
+
+async function fetchDetailedPokemons(baseData) {
+    const promises = baseData.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const detail = await response.json();
+        return {
+            name: detail.name,
+            image: detail.sprites.other["official-artwork"].front_default,
+            types: detail.types.map(t => t.type.name),
+            abilities: detail.abilities.map(a => a.ability.name),
+            id: detail.id,
+            color: detail.color,
+            weight: detail.weight,
+            height: detail.height,
+            location:detail.location_area_encounters,
+
+        };
+    });
+
+    allPokemons = await Promise.all(promises);
 }
 
 function renderPokemons() {
-    let pokemonList = document.getElementById('pokemon-list');
-    
-    // abilities ist ein Array
-    let all = allPokemons.results;
+    const container = document.getElementById('pokemon-list');
+    container.innerHTML = '';
 
-    for (let i = 0; i < all.length; i++) {
-        pokemonList.innerHTML += `<li>${all[i].name}</li>`;
-    }
+    allPokemons.forEach(pokemon => {
+        container.innerHTML += `
+            <div class="card" onclick="showDetails(${pokemon.id})">
+                <img src="${pokemon.image}" alt="${pokemon.name}">
+                <h3>${pokemon.name} - ${pokemon.stats}</h3>
+                <p> ${pokemon.color}</p>
+                <p>Typ: ${pokemon.types.join(', ')}</p>
+                <p>location: ${pokemon.location} </p>
+                <p>weight: ${pokemon.weight}</p>
+                <p>height: ${pokemon.height}</p>
+
+            </div>
+        `;
+    });
+}
+
+function showDetails(id) {
+    const pokemon = allPokemons.find(p => p.id === id);
+    alert(`Details zu ${pokemon.name}\nFähigkeiten: ${pokemon.abilities.join(', ')}`);
 }
