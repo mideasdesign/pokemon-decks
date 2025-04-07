45fc79ff3aab049ad566738b98513c4a5e4104c3
Markus Fischer <fischer@mideasdesign.de>
2025-04-07 19:14:30 +0200

    add load more functionality

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,5 +1,13 @@
-function getCardTemplate(){
+function getBaseCardTemplate(pokemon){
     return /* html */`
-        <div class="grid grid-flow"></div>
+        <div class="card" onclick="showDetails(${pokemon.id})">
+            <img src="${pokemon.image}" alt="${pokemon.name}">
+            <h3 class="${pokemon.types[0]}">${pokemon.name} - #${pokemon.id}</h3>
+            <p>Typ: ${pokemon.types.join(', ')}</p>
+        </div>
     `
+};
+function showDetails(id) {
+    const pokemon = allPokemons.find(p => p.id === id);
+    alert(`Details zu ${pokemon.name}\nFähigkeiten: ${pokemon.abilities} <p> ${pokemon.statName}: ${pokemon.base}</p>`);
 }
--- script.js
+++ script.js
@@ -1,39 +1,14 @@
 let allPokemons = [];
-let flavorTextRef = `https://pokeapi.co/api/v2/pokemon-species?offset=0&limit=20`
+let pageOffset = 0;
 
 async function init() {
     const baseData = await fetchBasePokemons();
     await fetchBaseDataPokemons(baseData);
-
-    const flavorTextData = await  fetchFlavorText();
-    await fetchBaseDataPokemons(flavorTextData);
     renderBaseCardPokemons();
 }
 
-async function fetchFlaverText() {
-    const response = await fetch(flavorTextRef);
-    const flavorTextData = await response.json();
-    return flavorTextData.results; // Array mit { name, url }
-}
-
-async function fetchFlavorTextData(flavorTextData) {
-    const promises = flavorTextData.map(async (pokemon) => {
-        const response = await fetch(pokemon.url);
-        const flavorDetail = await response.json();
-        return {
-            name: detail.name,
-            image: detail.sprites.other["official-artwork"].front_default,
-            types: detail.types.map(t => t.type.name),
-            abilities: detail.abilities.map(a => a.ability.name),
-            id: detail.id,
-        };
-    });
-
-    allPokemons = await Promise.all(promises);
-}
-
 async function fetchBasePokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pageOffset}&limit=20`);
     const data = await response.json();
     return data.results; // Array mit { name, url }
 }
@@ -50,57 +25,40 @@ async function fetchBaseDataPokemons(baseData) {
             id: detail.id,
         };
     });
-    allPokemons = await Promise.all(promises);
+    const newPokemons = await Promise.all(promises);
+    allPokemons.push(...newPokemons);
 }
-async function fetchStatsDataPokemons(){
-    const promises = baseData.map(async (pokemon) => {
-        const response = await fetch(pokemon.url);
-        const detail = await response.json();
-        return {
-            base: detail.stats.map(s => s.base_stat),
-            statName: detail.stats.map(s => s.name),
-            id: detail.id,
-        };
+
+function renderBaseCardPokemons() {
+    const container = document.getElementById('pokemon-list');
+    container.innerHTML = ''; // Beim ersten Laden Liste leeren
+    allPokemons.forEach(pokemon => {
+        container.innerHTML += getBaseCardTemplate(pokemon);
     });
 }
 
-async function fetchDetaildDataPokemons(baseData) {
+async function loadMorePokemons() {
+/*     if (pageOffset >= 100) return; */
+
+    pageOffset += 20;
+    const baseData = await fetchBasePokemons();
     const promises = baseData.map(async (pokemon) => {
         const response = await fetch(pokemon.url);
         const detail = await response.json();
         return {
             name: detail.name,
             image: detail.sprites.other["official-artwork"].front_default,
-            base: detail.stats.map(s => s.base_stat),
-            statName: detail.stats.map(s => s.name),
+            types: detail.types.map(t => t.type.name),
             abilities: detail.abilities.map(a => a.ability.name),
             id: detail.id,
-            weight: detail.weight,
-            height: detail.height,
         };
     });
 
-    allPokemons = await Promise.all(promises);
-}
-function renderBaseCardPokemons() {
+    const newPokemons = await Promise.all(promises);
+    allPokemons.push(...newPokemons);
+
     const container = document.getElementById('pokemon-list');
-    container.innerHTML = '';
-
-    allPokemons.forEach(pokemon => {
-        container.innerHTML += `
-            <div class="card" onclick="showDetails(${pokemon.id})">
-                <img src="${pokemon.image}" alt="${pokemon.name}">
-                <h3 class="${pokemon.types[0]}">${pokemon.name} - #${pokemon.id}</h3>
-                <p> ${pokemon.statName}: ${pokemon.base}</p>
-                <p>Typ: ${pokemon.types.join(', ')}</p>
-            </div>
-        `;
+    newPokemons.forEach(pokemon => {
+        container.innerHTML += getBaseCardTemplate(pokemon);
     });
-}
-
-function render
-
-function showDetails(id) {
-    const pokemon = allPokemons.find(p => p.id === id);
-    alert(`Details zu ${pokemon.name}\nFähigkeiten: ${pokemon.abilities}`);
 }
